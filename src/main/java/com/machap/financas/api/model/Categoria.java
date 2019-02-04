f948203b3ecdbc859e0d1c544eaf7ebc22434dff
machap <paulinhodj10@gmail.com>
2019-02-04 14:44:24 -0300

    Troca de Id por Codigo

--- src/main/java/com/machap/financas/api/model/Categoria.java
+++ src/main/java/com/machap/financas/api/model/Categoria.java
@@ -11,16 +11,16 @@ import javax.persistence.Table;
 public class Categoria {
 	@Id
 	@GeneratedValue(strategy = GenerationType.IDENTITY)
-	private Long id;
+	private Long codigo;
 	private String nome;
 
-	public Long getId() {
-		return id;
+	public Long getCodigo() {
+		return codigo;
 	}
 
-	public void setId(Long id) {
-		this.id = id;
-	}
+//	public void setCodigo(Long codigo) {
+//		this.codigo = codigo;
+//	}
 
 	public String getNome() {
 		return nome;
@@ -34,7 +34,7 @@ public class Categoria {
 	public int hashCode() {
 		final int prime = 31;
 		int result = 1;
-		result = prime * result + ((id == null) ? 0 : id.hashCode());
+		result = prime * result + ((codigo == null) ? 0 : codigo.hashCode());
 		return result;
 	}
 
@@ -47,10 +47,10 @@ public class Categoria {
 		if (getClass() != obj.getClass())
 			return false;
 		Categoria other = (Categoria) obj;
-		if (id == null) {
-			if (other.id != null)
+		if (codigo == null) {
+			if (other.codigo != null)
 				return false;
-		} else if (!id.equals(other.id))
+		} else if (!codigo.equals(other.codigo))
 			return false;
 		return true;
 	}
