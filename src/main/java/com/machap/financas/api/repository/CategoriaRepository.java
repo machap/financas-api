dec38c531a32bdb1301d509edf31eba4b8631532
machap <paulinhodj10@gmail.com>
2019-02-04 14:41:57 -0300

    Crianção do repository para categoria

---
+++ src/main/java/com/machap/financas/api/repository/CategoriaRepository.java
@@ -0,0 +1,9 @@
+package com.machap.financas.api.repository;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import com.machap.financas.api.model.Categoria;
+
+public interface CategoriaRepository extends JpaRepository<Categoria, Long>{
+
+}
