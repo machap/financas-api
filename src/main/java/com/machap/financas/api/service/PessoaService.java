a42ca5d4ad54f070c934e81fbf1567c7c0887676
machap <paulinhodj10@gmail.com>
2019-02-05 17:30:59 -0300

    Criando Camada de servico para pessoa

---
+++ src/main/java/com/machap/financas/api/service/PessoaService.java
@@ -0,0 +1,24 @@
+package com.machap.financas.api.service;
+
+import org.springframework.beans.BeanUtils;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.dao.EmptyResultDataAccessException;
+import org.springframework.stereotype.Service;
+
+import com.machap.financas.api.model.Pessoa;
+import com.machap.financas.api.repository.PessoaRepository;
+
+@Service
+public class PessoaService {
+
+	@Autowired
+	private PessoaRepository pessoaRepository;
+
+	public Pessoa atualizar(Long codigo, Pessoa pessoa) {
+		Pessoa pessoaSalva = pessoaRepository.findOne(codigo);
+		if (pessoaSalva == null)
+			throw new EmptyResultDataAccessException(1);
+		BeanUtils.copyProperties(pessoa, pessoaSalva, "codigo");
+		return pessoaRepository.save(pessoaSalva);
+	}
+}
