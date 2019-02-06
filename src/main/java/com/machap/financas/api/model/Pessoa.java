330555d6458c917e673f15d525fe3035d5020945
machap <paulinhodj10@gmail.com>
2019-02-06 11:28:12 -0300

    Tratando execao para pessoa nao encontrada em lancamento

--- src/main/java/com/machap/financas/api/model/Pessoa.java
+++ src/main/java/com/machap/financas/api/model/Pessoa.java
@@ -6,7 +6,10 @@ import javax.persistence.GeneratedValue;
 import javax.persistence.GenerationType;
 import javax.persistence.Id;
 import javax.persistence.Table;
+import javax.persistence.Transient;
 import javax.validation.constraints.NotNull;
+
+import com.fasterxml.jackson.annotation.JsonIgnore;
 
 @Entity
 @Table(name = "pessoa")
@@ -52,6 +55,12 @@ public class Pessoa {
 		return codigo;
 	}
 
+	@JsonIgnore
+	@Transient
+	public boolean isInativo() {
+		return !this.ativo;
+	}
+
 	@Override
 	public int hashCode() {
 		final int prime = 31;
@@ -76,5 +85,4 @@ public class Pessoa {
 			return false;
 		return true;
 	}
-
 }
