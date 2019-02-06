075b5d6d52ae24a1cc5d1391b5ccb7eb6bd14c21
machap <paulinhodj10@gmail.com>
2019-02-06 19:47:33 -0300

    Mudando o retorno do metodo busca de lancamento, para usar peginacao

--- src/main/java/com/machap/financas/api/repository/lancamento/LancamentoRepositoryQuery.java
+++ src/main/java/com/machap/financas/api/repository/lancamento/LancamentoRepositoryQuery.java
@@ -1,10 +1,11 @@
 package com.machap.financas.api.repository.lancamento;
 
-import java.util.List;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
 
 import com.machap.financas.api.model.Lancamento;
 import com.machap.financas.api.repository.filter.LancamentoFilter;
 
 public interface LancamentoRepositoryQuery {
-	public List<Lancamento> filtrar(LancamentoFilter filter);
+	public Page<Lancamento> filtrar(LancamentoFilter filter, Pageable pageable);
 }
