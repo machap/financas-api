e642213a2a83a78320fe7f025bf3a8f2608ea7d9
machap <paulinhodj10@gmail.com>
2019-02-05 21:48:14 -0300

    Incluindo bean validation para lancamento

--- src/main/java/com/machap/financas/api/model/Lancamento.java
+++ src/main/java/com/machap/financas/api/model/Lancamento.java
@@ -13,6 +13,7 @@ import javax.persistence.Id;
 import javax.persistence.JoinColumn;
 import javax.persistence.ManyToOne;
 import javax.persistence.Table;
+import javax.validation.constraints.NotNull;
 
 @Entity
 @Table(name = "lancamento")
@@ -20,25 +21,32 @@ public class Lancamento {
 	@Id
 	@GeneratedValue(strategy = GenerationType.IDENTITY)
 	private Long codigo;
+	
+	@NotNull
 	private String descricao;
 
 	@Column(name = "data_vencimento")
+	@NotNull
 	private LocalDate dataVencimento;
 
 	@Column(name = "data_pagamento")
 	private LocalDate dataPagamento;
 
+	@NotNull
 	private BigDecimal valor;
 	
 	@Enumerated(EnumType.STRING)
+	@NotNull
 	private TipoLancamento tipo;
 
 	@ManyToOne
 	@JoinColumn(name = "codigo_categoria")
+	@NotNull
 	private Categoria categoria;
 
 	@ManyToOne
 	@JoinColumn(name = "codigo_pessoa")
+	@NotNull
 	private Pessoa pessoa;
 
 	public Long getCodigo() {
